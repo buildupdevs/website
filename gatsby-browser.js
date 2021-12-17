c32371cc73116d05d97d80339e4663de33f9fa3d
Dhanish Gajjar <dhanish.b.gajjar@gmail.com>
2021-12-17 08:52:33 +0000

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Layout from './src/components/Layout';
-
-export function wrapPageElement({ element, props }) {
-  /* eslint-disable react/jsx-props-no-spreading */
-  return <Layout {...props}>{element}</Layout>;
-}
--- src/styles/FooterStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterStyles = styled.div`
-  color: #555;
-  margin-top: 2rem;
-  p {
-    font-size: 0.75rem;
-  }
-`;
